1a6c31e015d258b8e2299c644fa97418fb851b02
Jaagup Kuhi <jaagupku@gmail.com>
2017-11-18 18:14:22 +0200

    added noise generator and looking around with camera

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject target;
+    private float x, y;
+    private float distance;
+
+	// Use this for initialization
+	void Start () {
+        x = 0.0f;
+        y = 0.0f;
+        distance = 10;
+
+        Vector3 vector = new Vector3(0, 0, 1);
+        vector = Quaternion.Euler(y, x, 0) * vector;
+        transform.position = target.transform.position + (vector * distance);
+
+        transform.LookAt(target.transform);
+        transform.LookAt(target.transform);
+    }
+
+    void updateCameraPosition()
+    {
+        Vector3 vector = new Vector3(0, 0, 1);
+        vector = Quaternion.Euler(y, x, 0) * vector;
+        transform.position = target.transform.position + (vector * distance);
+
+        transform.LookAt(target.transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            distance -= distance/10.0f;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            distance += distance / 10.0f;
+        }
+
+        if (distance < 2.5)
+            distance = 2.5f;
+        else if (distance > 40)
+            distance = 40;
+
+        if (Input.GetMouseButton(1))
+        {
+            x += Input.GetAxis("Mouse X") * 5;
+            y += Input.GetAxis("Mouse Y") * 5;
+
+            if (y > 89.9f)
+                y = 89.9f;
+            else if (y < -89.9f)
+                y = -89.9f;
+            if (x > 360 || x < -360)
+                x = Mathf.Abs(x) - 360;
+        }
+
+        updateCameraPosition();
+    }
+}
---
+++ Assets/Shaders/volumetricFire.shader
@@ -0,0 +1,64 @@
+﻿Shader "Unlit/volumetricFire"
+{
+	Properties
+	{
+		_Freq("Frequency", Float) = 1
+		_Speed("Speed", Float) = 1
+		
+	}
+	SubShader
+	{
+		Tags { "Queue" = "Transparent" }
+		LOD 100
+
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			#include "noiseSimplex.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				float3 srcPos : TEXCOORD0;
+			};
+
+			uniform float
+				_Freq,
+				_Speed
+			;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+
+				o.pos = UnityObjectToClipPos(v.vertex);
+
+				o.srcPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+				o.srcPos *= _Freq;
+				o.srcPos.y += _Time.y * _Speed;
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				float ns = snoise(i.srcPos) / 2 + 0.5f;
+				return float4(ns, ns, ns, 1.0f);
+			}
+			ENDCG
+		}
+	}
+}
